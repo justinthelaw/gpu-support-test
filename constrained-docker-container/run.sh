cc55d0534d0d1d932623a8f370eed8978f3a26f2
justinthelaw <justinthelaw@gmail.com>
2023-10-25 11:51:55 -0400

    added constrained environment
    Signed-off-by: justinthelaw <justin.law@defenseunicorns.com>

---
+++ constrained-docker-container/run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+docker build -t airgap-ubuntu-2204 . && \
+
+docker run --rm -it \
+  --memory=64g \
+  --gpus device=0 \
+  -v volume:/container/storage \
+  --shm-size=128g \
+  --network=none \
+  airgap-ubuntu-2204
