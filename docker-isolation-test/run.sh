4fb38bf4074d63d6940b81b23cf0cf4f1d092b90
justinthelaw <justinthelaw@gmail.com>
2023-11-09 18:55:27 -0500

    installation instructions updated

--- docker-isolation-test/run.sh
+++ docker-isolation-test/run.sh
@@ -3,9 +3,10 @@
 docker build -t airgap-ubuntu-2204 . && \
 
 docker run -it \
+  -privilaged \
   --memory=64g \
   --gpus device=0 \
-  -v volume:/container/storage \
   --shm-size=128g \
   --network=none \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   airgap-ubuntu-2204
