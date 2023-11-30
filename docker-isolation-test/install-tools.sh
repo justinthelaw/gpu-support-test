d3f09e309b8ffea4bca3611ec065ee8efa15af58
justinthelaw <justinthelaw@gmail.com>
2023-11-30 11:16:21 -0500

    back to just GPU testing
    Signed-off-by: justinthelaw <justin.law@defenseunicorns.com>

--- docker-isolation-test/install-tools.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-mv tools/zarf_v0.31.0_Linux_amd64 /usr/local/bin/zarf && chmod +x /usr/local/bin/zarf && zarf version
-
-install -o root -g root -m 0755 tools/kubectl /usr/local/bin/kubectl && kubectl version
-
-mv tools/k3d-linux-amd64 /usr/local/bin/k3d && chmod +x /usr/local/bin/k3d && k3d version
