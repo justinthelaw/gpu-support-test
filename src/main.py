0bcad268adad3a3ea404f29b2a519f5d62c5b612
justinthelaw <justin.law@defenseunicorns.com>
2024-01-09 15:59:30 -0500

    improved python script execution
    Signed-off-by: justinthelaw <justin.law@defenseunicorns.com>

--- src/main.py
+++ src/main.py
@@ -1,18 +1,39 @@
 import torch
 import time
+import signal
+import sys
+
+
+def sigterm_handler(signum, frame):
+    sys.exit(0)
+
+
+signal.signal(signal.SIGTERM, sigterm_handler)
+
+start_timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+
+print(f"{start_timestamp} => Commencing GPU availability test...")
 
 while True:
-    print("Commencing GPU availability test...\n")
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
 
-    cuda_available = torch.cuda.is_available()
-    if cuda_available:
-        print("GPU(s) are accessible to this container! See list of available devices below:")
-        device_count = torch.cuda.device_count()
-        for device in range(device_count):
-            print(f"\tDevice #{device}: {torch.cuda.get_device_name(device)}")
-    else:
-        print("GPU(s) are NOT accessible to this container!")
+    try:
+        cuda_available = torch.cuda.is_available()
+        if cuda_available:
+            print(
+                f"{timestamp} => GPU(s) are accessible to this container! See list of available devices below:"
+            )
+            device_count = torch.cuda.device_count()
+            for device in range(device_count):
+                print(
+                    f"{timestamp} => \tDevice #{device}: {torch.cuda.get_device_name(device)}"
+                )
+        else:
+            print(f"{timestamp} => GPU(s) are NOT accessible to this container!")
 
-    print("\nGPU availability test has ended.")
-    
-    time.sleep(10)
+        time.sleep(10)
+
+    except KeyboardInterrupt:
+        print(f"\n{timestamp} => KeyboardInterrupt (Ctrl-C) detected.")
+        print(f"{timestamp} => GPU availability test exited gracefully.")
+        sys.exit(0)
